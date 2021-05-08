86ec8db5ed64ef8b295443a293b1d541407da5aa
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2021-05-07 23:28:59 -0400

    fix: application route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :lists do
+    resources :items
+  end
+  namespace :list do
+    resources :items
+  end
+  get 'about/index'
+  devise_for :users
   root "home#index"
 end
