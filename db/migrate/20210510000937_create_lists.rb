a36b1d3fc736de0fa4d24edef81d5d479f1ae70d
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2021-05-10 01:44:52 -0400

    fix: edit users, lists and items on db

--- db/migrate/20210510000937_create_lists.rb
+++ db/migrate/20210510000937_create_lists.rb
@@ -1,8 +1,8 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-      t.string :title
       t.references :user, null: false, foreign_key: true
+      t.string :title
 
       t.timestamps
     end
